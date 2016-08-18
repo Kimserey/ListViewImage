be7fe2f2e0a52f6c617f3ce3216a122f5ce5ef5f
Kimserey Lam <kimserey.lam@gmail.com>
2016-08-18 23:52:47 +0100

    more layout

--- ListViewImage/ListViewImage.cs
+++ ListViewImage/ListViewImage.cs
@@ -9,64 +9,77 @@ using FFImageLoading.Transformations;
 
 namespace ListViewImage
 {
+	public class Store { 
+		public string Name { get; set; }
+		public string Image { get; set; }
+	}
+
 	public class App : Application
 	{
 		public App()
 		{
-			// The root page of your application
+			var list = new ListView
+			{
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+				VerticalOptions = LayoutOptions.FillAndExpand,
+				ItemsSource = new List<Store> {
+					new Store { Image = "aldi.jpg", Name = "Aldi" },
+					new Store { Image = "asda.jpg", Name = "Asda" },
+					new Store { Image = "tesco.jpg", Name = "Tesco" },
+					new Store { Image = "waitrose.jpg", Name = "Waitrose" }
+				},
+				RowHeight = 200,
+				ItemTemplate = new DataTemplate(() =>
+				{
+					var t = new AbsoluteLayout
+					{
+						HorizontalOptions = LayoutOptions.FillAndExpand,
+						VerticalOptions = LayoutOptions.FillAndExpand,
+					};
+
+					var image = new CachedImage
+					{
+						HorizontalOptions = LayoutOptions.FillAndExpand,
+						HeightRequest = 200,
+						DownsampleToViewSize = true,
+						TransparencyEnabled = false,
+						Aspect = Aspect.AspectFill,
+						TransformPlaceholders = false,
+						LoadingPlaceholder = "icon.png",
+						ErrorPlaceholder = "icon.png"
+					};
+
+					var label = new Label
+					{
+						TextColor = Color.White,
+						BackgroundColor = Color.Purple,
+						VerticalTextAlignment = TextAlignment.Center,
+						HorizontalTextAlignment = TextAlignment.Center
+					};
+
+					label.SetBinding(Label.TextProperty, "Name");
+					image.SetBinding(CachedImage.SourceProperty, "Image");
+
+					t.Children.Add(image, new Rectangle(.5, .5, .9, .9), AbsoluteLayoutFlags.All);
+					t.Children.Add(label, new Rectangle(1, .8, .5, .2), AbsoluteLayoutFlags.All);
+					return new ViewCell { View = t };
+				}),
+				SeparatorVisibility = SeparatorVisibility.None
+			};
+
+			if (Device.OS == TargetPlatform.iOS || Device.OS == TargetPlatform.Android) {
+				list.ItemTapped += (sender, e) => {
+					list.SelectedItem = null;
+				};
+			}
+
 			var content = new ContentPage
 			{
 				Title = "ListViewImage",
-				Content = new ListView
-				{
-					HorizontalOptions = LayoutOptions.FillAndExpand,
-					VerticalOptions = LayoutOptions.FillAndExpand,
-					ItemsSource = Enumerable.Range(0, 100).Select(i => "image.jpg"),
-					RowHeight = 300,
-					ItemTemplate = new DataTemplate(() => {
-						var t = new AbsoluteLayout {
-							HorizontalOptions = LayoutOptions.FillAndExpand,
-							VerticalOptions = LayoutOptions.FillAndExpand,
-							Margin = new Thickness(10)
-						};
-
-						var image = new CachedImage {
-							HorizontalOptions = LayoutOptions.FillAndExpand,
-							HeightRequest = 300,
-							DownsampleToViewSize = true,
-							TransparencyEnabled = false,
-							Aspect = Aspect.AspectFill,
-							CacheDuration = TimeSpan.FromDays(30),
-							TransformPlaceholders = false,
-							LoadingPlaceholder = "icon.png",
-							ErrorPlaceholder = "icon.png"
-						};
-
-						image.SetBinding(CachedImage.SourceProperty, ".");
-
-						t.Children.Add(image, new Rectangle(.5, .5, 1, 1), AbsoluteLayoutFlags.All);
-						return new ViewCell { View = t }; 
-					}),
-					SeparatorVisibility = SeparatorVisibility.None
-				}
+				Content = list
 			};
 
 			MainPage = new NavigationPage(content);
-		}
-
-		protected override void OnStart()
-		{
-			// Handle when your app starts
-		}
-
-		protected override void OnSleep()
-		{
-			// Handle when your app sleeps
-		}
-
-		protected override void OnResume()
-		{
-			// Handle when your app resumes
 		}
 	}
 }
